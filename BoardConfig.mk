3eb037d7e64a662aee9fd6c6a0c4162cbf8259a5
trivalent <rahulshukla4u@gmail.com>
2014-01-15 23:37:40 +0530

    ReInit

---
+++ BoardConfig.mk
@@ -0,0 +1,43 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/kyleve/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := kyleve
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x82000000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x800000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x800000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 943718400
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2638217216
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/samsung/kyleve/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+#Audio and Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+OPENSOURCE_ALSA_AUDIO := true
+BOARD_USES_ALSA_AUDIO := true
+
---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := kyleve
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/kyleve/device_kyleve.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kyleve
+PRODUCT_NAME := cm_kyleve
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := kyleve
+PRODUCT_MANUFACTURER := samsung
