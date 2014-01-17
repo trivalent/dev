c0640f2b96876a5c5246a228cc18f6539ec60df7
trivalent <rahulshukla4u@gmail.com>
2014-01-17 16:00:11 +0530

    Trying to bring up radio

--- device_kyleve.mk
+++ device_kyleve.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 device/samsung/kyleve/init.hawaii_ss_kyleve.rc:root/init.hawaii_ss_kyleve.rc \
 device/samsung/kyleve/init.bcm2166x.usb.rc:root/init.bcm2166x.usb.rc \
+device/samsung/kyleve/init.bt.rc:root/init.bt.rc \
 device/samsung/kyleve/init.log.rc:root/init.log.rc \
 device/samsung/kyleve/init.recovery.kyleve.rc:root/init.recovery.kyleve.rc \
 device/samsung/kyleve/lpm.rc:root/lpm.rc \
@@ -70,14 +71,53 @@ PRODUCT_COPY_FILES += \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 
-# Device GLES libs
+# Device Device libs and Modules
 PRODUCT_COPY_FILES += \
 	device/samsung/kyleve/prop/lib/audio.primary.hawaii.so:system/lib/hw/audio.primary.hawaii.so \
 	device/samsung/kyleve/prop/lib/libGLES_hawaii.so:system/lib/egl/libGLES_hawaii.so \
 	device/samsung/kyleve/prop/lib/gralloc.hawaii.so:system/lib/hw/gralloc.hawaii.so \
 	device/samsung/kyleve/prop/lib/hwcomposer.hawaii.so:system/lib/hw/hwcomposer.hawaii.so \
+	device/samsung/kyleve/prop/lib/gps.hawaii.so:system/lib/hw/gps.hawaii.so \
 	device/samsung/kyleve/prop/lib/libuip.so:system/lib/libuip.so \
-	device/samsung/kyleve/prop/lib/libVCOS.so:system/lib/libVCOS.so
+	device/samsung/kyleve/prop/lib/libVCOS.so:system/lib/libVCOS.so \
+	device/samsung/kyleve/prop/lib/libuecodec.so:system/lib/libuecodec.so \
+	device/samsung/kyleve/prop/lib/libasound.so:system/lib/libasound.so \
+	device/samsung/kyleve/prop/lib/libat.so:system/lib/libat.so \
+	device/samsung/kyleve/prop/lib/libatlog.so:system/lib/libatlog.so \
+	device/samsung/kyleve/prop/lib/libat_stubs.so:system/lib/libat_stubs.so \
+	device/samsung/kyleve/prop/lib/libatparser.so:system/lib/libatparser.so \
+	device/samsung/kyleve/prop/lib/libbrcmcutils.so:system/lib/libbrcmcutils.so \
+	device/samsung/kyleve/prop/lib/libbrcmparser.so:system/lib/libbrcmparser.so \
+	device/samsung/kyleve/prop/lib/libcapi2.so:system/lib/libcapi2.so \
+	device/samsung/kyleve/prop/lib/libfactoryutil.so:system/lib/libfactoryutil.so \
+	device/samsung/kyleve/prop/lib/libomission_avoidance.so:system/lib/libomission_avoidance.so \
+	device/samsung/kyleve/prop/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
+	device/samsung/kyleve/prop/lib/libsecril-client.so:system/lib/libsecril-client.so \
+	device/samsung/kyleve/prop/lib/libsoc.so:system/lib/libsoc.so \
+	device/samsung/kyleve/prop/lib/libbrcm_ril.so:system/lib/libbrcm_ril.so \
+	device/samsung/kyleve/prop/lib/librpc.so:system/lib/librpc.so \
+	device/samsung/kyleve/prop/lib/libusb_config.so:system/lib/libusb_config.so \
+	device/samsung/kyleve/prop/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
+	device/samsung/kyleve/config/usb_portd.conf:system/etc/usb_portd.conf \
+	device/samsung/kyleve/prop/etc/gps/glconfig.xml:system/etc/gps/glconfig.xml \
+	
+
+# Device Device BINS
+PRODUCT_COPY_FILES += \
+	device/samsung/kyleve/prop/bin/BCM4330B1.hcd:system/bin/BCM4330B1.hcd \
+	device/samsung/kyleve/prop/bin/at_distributor:system/bin/at_distributor \
+	device/samsung/kyleve/prop/bin/atx:system/bin/atx \
+	device/samsung/kyleve/prop/bin/atxd:system/bin/atxd \
+	device/samsung/kyleve/prop/bin/bcm_dut:system/bin/bcm_dut \
+	device/samsung/kyleve/prop/bin/btld:system/bin/btld \
+	device/samsung/kyleve/prop/bin/glgps:system/bin/glgps \
+	device/samsung/kyleve/prop/bin/gps.cer:system/bin/gps.cer \
+	device/samsung/kyleve/prop/bin/macloader:system/bin/macloader \
+	device/samsung/kyleve/prop/bin/mfgloader:system/bin/mfgloader \
+	device/samsung/kyleve/prop/bin/mttlogger:system/bin/mttlogger \
+	device/samsung/kyleve/prop/bin/rild:system/bin/rild \
+	device/samsung/kyleve/prop/bin/usb_portd:system/bin/usb_portd \
+	
 	
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.ril_class=SamsungBCMRIL \
