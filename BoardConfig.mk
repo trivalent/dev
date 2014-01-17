92e27443326707e0ef205b4baac5f738a384fe52
Rahul SHukla <rahul.sk@samsung.com>
2014-01-17 09:33:29 +0530

    Board Compilation Success, a lot of crashes Like 	SystemUI crashed 	com.android.phone etc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,30 +4,46 @@ USE_CAMERA_STUB := true
 -include vendor/samsung/kyleve/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_BOARD_PLATFORM := hawaii
 TARGET_BOOTLOADER_BOARD_NAME := kyleve
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+#MULTIPLE DEVICE NAMES
+TARGET_OTA_ASSERT_DEVICE  := hawaii,kyleve,hawaii_ss_kyleve,kylevexx
+
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x82000000
 BOARD_KERNEL_PAGESIZE := 4096
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Allow to format EXT4 based systems
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_CACHE_DEVICE := /dev/block/mmcblk0p16
+BOARD_CACHE_FILESYSTEM := ext4
+BOARD_CACHE_FILESYSTEM_OPTIONS := rw
+BOARD_SYSTEM_DEVICE := /dev/block/mmcblk0p17
+BOARD_SYSTEM_FILESYSTEM := ext4
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := rw
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p19
+BOARD_DATA_FILESYSTEM := ext4
+BOARD_DATA_FILESYSTEM_OPTIONS := rw
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x800000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x800000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 943718400
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2638217216
-BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FLASH_BLOCK_SIZE := 0x00020000
 
 TARGET_PREBUILT_KERNEL := device/samsung/kyleve/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
-# Use this flag if the board has a ext4 partition larger than 2gb
-BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Hardware rendering
 USE_OPENGL_RENDERER := true
@@ -41,3 +57,29 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 OPENSOURCE_ALSA_AUDIO := true
 BOARD_USES_ALSA_AUDIO := true
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/kyleve/recovery.fstab
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE_REV       := bcm4330
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   := 802_11_ABG
+
+# RIL
+BOARD_RIL_CLASS := device/samsung/kyleve/ril/
--- device_kyleve.mk
+++ device_kyleve.mk
@@ -5,6 +5,9 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/kyleve/kyleve-vendor.mk)
 
+# Use high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
 DEVICE_PACKAGE_OVERLAYS += device/samsung/kyleve/overlay
 
 LOCAL_PATH := device/samsung/kyleve
@@ -17,19 +20,69 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
+# Init files
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/init.hawaii_ss_kyleve.rc:root/init.hawaii_ss_kyleve.rc \
+device/samsung/kyleve/init.bcm2166x.usb.rc:root/init.bcm2166x.usb.rc \
+device/samsung/kyleve/init.log.rc:root/init.log.rc \
+device/samsung/kyleve/init.recovery.kyleve.rc:root/init.recovery.kyleve.rc \
+device/samsung/kyleve/lpm.rc:root/lpm.rc \
+device/samsung/kyleve/ueventd.hawaii_ss_kyleve.rc:root/ueventd.hawaii_ss_kyleve.rc \
+device/samsung/kyleve/fstab.hawaii_ss_kyleve:root/fstab.hawaii_ss_kyleve \
+
+# Prebuilt kl keymaps
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/bcm_headset.kl:system/usr/keylayout/bcm_headset.kl \
+device/samsung/kyleve/bcm_keypad_v2.kl:system/usr/keylayout/bcm_keypad_v2.kl \
+device/samsung/kyleve/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+device/samsung/kyleve/samsung-keypad.kl:system/usr/keylayout/samsung-keypad.kl
+
+# Prebuilt kcm keychars
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/bcm_keypad_v2.kcm:system/usr/keychars/bcm_keypad_v2.kcm
+
+# Prebuilt icu
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/icudt48l.dat:system/usr/icu/icudt48l.dat
+
+# Prebuilt idc
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=SamsungBCMRIL
+	
+# Dalvik heap config
+include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+#$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_kyleve
 PRODUCT_DEVICE := kyleve
-
-
-# Prebuilt kl keymaps
-PRODUCT_COPY_FILES += \
-        device/samsung/kyleve/bcm_headset.kl:system/usr/keylayout/bcm_headset.kl \
-        device/samsung/kyleve/bcm_keypad_v2.kl:system/usr/keylayout/bcm_keypad_v2.kl \
-        device/samsung/kyleve/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-                device/samsung/kyleve/qwerty.kl:system/usr/keylayout/qwerty.kl \
-                device/samsung/kyleve/samsung-keypad.kl:system/usr/keylayout/samsung-keypad.kl
-
-
